56e1110ebcd5bc71fdf8e74caa1ed2f5d7e323e6
Nash-Skinner <nash.skinner@yahoo.com>
2022-04-20 02:24:44 -0500

    I'm sorry, but everything is different on the webpage

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import config from './config.json'; //assert{ type: "json" };
+import config from './config.json' assert{ type: "json" };
 import { initializeDatabase, useDatabase } from './initializeDatabase.js';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import mysql from 'mysql';
+import Game from './constructors/game.js';
 
 import { testDBOperations } from './testDatabaseOperations.js';
+import { response } from 'express';
 
 const db = mysql.createConnection({host: config.host, user: config.user, password: config.password});
 initializeDatabase(db, config).then(() => {
@@ -22,89 +24,36 @@ app
     .use(bodyParser.urlencoded({extended: true}))
     .use(bodyParser.json())
 
-    // Get Game Table
-    .get('/api/game', async(req,res) => {
-        var sql = "SELECT * FROM game ORDER BY gameId";
-        db.query(sql, (err, result)=>{
-            console.log(result)
-        })
+    .set('view engine', 'ejs')
+
+    // Get Games Table
+    .get('/api/game', (req,res) => {
+        db.query("SELECT * FROM game ORDER BY name", (err, data) => {
+            res.json(data);
+        });
     })
 
     // Post to Game Table
-    .post('/api/game', async(req, res) => {
+    .post('/api/game', async(req,res) => {
         const body = req.body;
+        const intGame = new Game(body.gameId, body.abbrev, body.gameName, body.releaseYear, body.weblink);
 
-        //await db.query(`            
-        const mySQLString = ("INSERT INTO game (gameId, abbrev, name, platforms, releaseYear, weblink) VALUES ('"+ body.gameId +"', '"+ body.abbrev +"', '"+ body.name +"', '"+ body.platforms +"', '"+ body.releaseYear +"', '"+ body.weblink +"')");
-        db.query(mySQLString)
-
-        res.redirect('..');
+        db.query(intGame.toSQL());
+        res.redirect('..')
     })
 
-    // Post to SteamGame Table
-    .post('/api/steamgame', async(req, res) => {
-        const body = req.body;
-
-        //await db.query(`            
-        const mySQLString = ("INSERT INTO steamgame (appId, srdcGameId, developer, releaseDate, publisher, rating) VALUES ('"+ body.appId +"', '"+ body.srdcGameId +"', '"+ body.developer +"', '"+ body.releaseDate +"', '"+ body.publisher +"', '"+ body.rating +"')");
-        db.query(mySQLString)
-
-        res.redirect('..');
+    // Get Category Table
+    .get('/api/category/:id', (req,res) => {
+        db.query(`SELECT * FROM category WHERE gameId = \'${req.params.id}\' ORDER BY categoryName`, (err, data) => {
+            res.json(data);
+        });
     })
 
-    // Post to Category Table
-    .post('/api/category', async(req, res) => {
-        const body = req.body;
-
-        //await db.query(`            
-        const mySQLString = ("INSERT INTO category (gameId, categoryId, categoryName, rules) VALUES ('"+ body.gameId +"', '"+ body.categoryId +"', '"+ body.categoryName +"', '"+ body.rules +"')");
-        db.query(mySQLString)
-
-        res.redirect('..');
-    })
-
-    // Post to Runner Table
-    .post('/api/runner', async(req, res) => {
-        const body = req.body;
-
-        //await db.query(`            
-        const mySQLString = ("INSERT INTO runner (userId, username, region, pronouns, joinDate) VALUES ('"+ body.userId +"', '"+ body.username +"', '"+ body.region +"', '"+ body.pronouns +"', '"+ body.joinDate +"')");
-        db.query(mySQLString)
-
-        res.redirect('..');
-    })
-
-    // Post to Run Table
-    .post('/api/run', async(req, res) => {
-        const body = req.body;
-
-        //await db.query(`            
-        const mySQLString = ("INSERT INTO run (runId, gameId, categoryId, runTime, placement, datePlayed, isObsolete, runDescription) VALUES ('"+ body.runId +"', '"+ body.gameId +"', '"+ body.categoryId +"', '"+ body.runTime +"', '"+ body.placement +"', '"+ body.datePlayed +"', '"+ body.isObsolete +"', '"+ body.runDescription +"')");
-        db.query(mySQLString)
-
-        res.redirect('..');
-    })
-
-    // Post to ModeratorFor Table
-    .post('/api/moderatorfor', async(req, res) => {
-        const body = req.body;
-
-        //await db.query(`            
-        const mySQLString = ("INSERT INTO moderatorfor (gameId, userId) VALUES ('"+ body.gameId +"', '"+ body.userId +"')");
-        db.query(mySQLString)
-
-        res.redirect('..');
-    })
-
-    // Post to PlayedBy Table
-    .post('/api/playedby', async(req, res) => {
-        const body = req.body;
-
-        //await db.query(`            
-        const mySQLString = ("INSERT INTO playedby (runId, gameId, categoryId, userId) VALUES ('"+ body.runId +"', '"+ body.gameId +"', '"+ body.categoryId +"', '"+ body.userId +"')");
-    	db.query(mySQLString)
-
-        res.redirect('..');
+    // Get Run Table
+    .get('/api/run/:id', (req,res) => {
+        db.query(`SELECT * FROM run WHERE categoryId = \'${req.params.id}\' ORDER BY placement`, (err, data) => {
+            res.json(data);
+        });
     })
 
     .listen(port, () => console.log(`Server Started on port ${port}`));
--- public/script.js
+++ public/script.js
@@ -1,15 +1,80 @@
-let tabs = document.querySelectorAll(".tabs h3");
-let tabContents = document.querySelectorAll(".tab-content div");
-
-tabs.forEach((tab,index) => {
-    tab.addEventListener("click", () => {
-        tabContents.forEach((content) => {
-            content.classList.remove("active");
-        });
-        tabs.forEach((tab) => {
-            tab.classList.remove("active");
-        });
-        tabContents[index].classList.add("active");
-        tabs[index].classList.add("active");
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('http://localhost:3000/api/game')
+    .then(res => res.json())
+    .then(data => loadGames(data));
 });
+
+// Load Game Data into nav-bar
+function loadGames(data) {
+    const tab = document.getElementById("nav-links");
+
+    if (data.length === 0) {
+        return;
+    } 
+
+    let tabHtml = "";
+
+    data.forEach(function ({gameId,name}){
+        tabHtml += `<li><a id=\"games\" onclick=\"loadCategory(\'${gameId}\')\">${name}</a></li>`;
+    });
+    tabHtml += `<li><a class="active" href="#gameForm" onclick=\"loadGameForm()\">New Game</a></li>`
+
+    tab.innerHTML = tabHtml;
+}
+
+// GameForm to Submit Games [visble : hidden]
+function loadGameForm() {
+    var node = document.getElementById("#gameForm");
+    var visibility = node.style.visibility;
+    node.style.visibility = visibility == "visible" ? 'hidden' : "visible"
+}
+
+// Get Category data for Game
+function loadCategory(gameId){
+    fetch(`http://localhost:3000/api/category/${gameId}`)
+    .then(res => res.json())
+    .then(data => loadCategories(data));
+};
+
+// Load Category into sidebar
+function loadCategories(data) {
+    const option = document.getElementById("sidenav");
+    
+    if (data.length === 0) {
+        return;
+    } 
+
+    let optionHtml = "";
+
+    data.forEach(function ({categoryId,categoryName}){
+        optionHtml += `<a id=\"category\" onclick=\"loadRun(\'${categoryId}\')\">${categoryName}</a>`;
+    });
+    option.innerHTML = optionHtml;
+}
+
+// Get Run data for Category
+function loadRun(categoryId){
+    fetch(`http://localhost:3000/api/run/${categoryId}`)
+    .then(res => res.json())
+    .then(data => loadRunTable(data));
+};
+
+// Load Run data into a table
+function loadRunTable(data) {
+    const table = document.getElementById("table");
+
+    if (data.length === 0) {
+        return;
+    } 
+
+    let tableHtml = "";
+
+    data.forEach(function ({runTime,placement,datePlayed}){
+        tableHtml += `<td border: 1px solid;></td>`;
+    });
+
+    console.log(tableHtml.innerHTML);
+
+    table.outerHTML = `<th border: 1px solid;>runTime</th><th border: 1px solid;>placement</th><th border: 1px solid;>datePlayed</th>`
+    table.innerHTML = tableHtml;
+}
